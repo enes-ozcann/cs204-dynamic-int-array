e010208eca727fe9d13261a9a23d70cede3774be
enes-ozcann <enesozcan0402@gmail.com>
2026-06-09 20:03:49 +0300

    Implemented DynamicIntArray

--- main.cpp
+++ main.cpp
@@ -1,6 +1,80 @@
 #include <iostream>
+using namespace std;
+
+class DynamicIntArray
+{
+private:
+    int* data;
+    int size;
+
+public:
+    DynamicIntArray(int s)
+    {
+        size = s;
+        data = new int[size];
+
+        for (int i = 0 ; i < size ; i++)
+        {
+            data[i] = 0;
+        }
+    }
+    ~DynamicIntArray()
+    {
+        delete [] data;
+    }
+
+    void set(int index, int value)
+    {
+        data[index] = value;
+    }
+    int get(int index) const
+    {
+        return data[index];
+    }
+    void print() const
+    {
+        for (int i = 0 ; i < size ; i++ )
+        {
+            if (i != size-1) {
+                cout << get(i) << ", ";
+            }
+            else
+            {
+                cout << get(i) << "." << endl;
+            }
+        }
+    }
+
+    int getSize() const
+    {
+        return size;
+    }
+
+    void fill(int value)
+    {
+        for (int i = 0 ; i < size ; i++)
+        {
+            data[i] = value;
+        }
+    }
+    int sum() const
+    {
+        int sum = 0;
+        for (int i = 0 ; i < size ; i++)
+        {
+            sum = sum + data[i];
+        }
+        return sum;
+    }
+};
+
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+
+
     return 0;
 }
